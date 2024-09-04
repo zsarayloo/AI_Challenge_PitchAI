83e8453888e40f398e816f12de9444c31fd96ff3
Zahra <zsaraylo@uwaterloo.ca>
2024-09-04 18:35:45 -0400

    modify test code and data

--- test_code.py
+++ test_code.py
@@ -1,4 +1,9 @@
 import unittest
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 from main_code import load_data, preprocess_data, train_model, evaluate_model
 
 class TestSpambaseChallenge(unittest.TestCase):
