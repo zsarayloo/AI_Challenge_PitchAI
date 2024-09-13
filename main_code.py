9f7eea0f3fdacc96ad1fbe3ea69faba4817ddb36
hpuranik <harshapuranik1704@gmail.com>
2024-09-13 19:14:29 -0400

    Built classification model on UCI Spambase dataset

--- main_code.py
+++ main_code.py
@@ -1,5 +1,6 @@
 # This challenge is designed for Wat.AI core member hiring process ( project name : Pitch.AI)
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
@@ -8,22 +9,44 @@ from sklearn.metrics import accuracy_score, classification_report
 def load_data(filepath):
     # Load the dataset from the file and return it as a DataFrame
     # TODO: Implement this function
-    pass
+    df = pd.read_csv(filepath)
+    # print(df.info())
+    # print(df.describe)
+    # print(df.head(10))
+    return df
 
 def preprocess_data(df):
     # Perform preprocessing like scaling features
     # TODO: Implement this function
-    pass
+
+    # Get features in X and target column in y
+    X = df.iloc[:,:-1]
+    y = df.iloc[:,-1]
+
+    # Scale the features
+    autoscaler = StandardScaler()
+    X_scaled = autoscaler.fit_transform(X)
+
+    # Add X_scaled and y back to a dataframe
+    df_scaled = pd.DataFrame(X_scaled, columns=df.columns[:-1])
+    df_scaled["target"] = y
+    return df_scaled
 
 def train_model(X_train, y_train):
     # Train a RandomForestClassifier on the provided data
     # TODO: Implement this function
-    pass
+    model = RandomForestClassifier(random_state=np.random.seed(42))
+    model.fit(X_train, y_train)
+    return model
 
 def evaluate_model(model, X_test, y_test):
     # Evaluate the trained model and return the accuracy and classification report
     # TODO: Implement this function
-    pass
+    y_preds = model.predict(X_test)
+    # acc_score = model.compare(y_preds, y_test)
+    acc_score = accuracy_score(y_preds, y_test)
+    clf_rep = classification_report(y_preds, y_test)
+    return acc_score, clf_rep
 
 if __name__ == "__main__":
     # Path to the dataset
