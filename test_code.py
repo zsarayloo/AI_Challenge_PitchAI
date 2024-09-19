4e190a1ed7870720ad8bcb0952e78818599aa733
zahra <zsaraylo@uwaterloo.ca>
2024-09-19 16:57:33 -0400

    Update test_code.py

--- test_code.py
+++ test_code.py
@@ -1,5 +1,6 @@
 import unittest
 import pandas as pd
+import os
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
